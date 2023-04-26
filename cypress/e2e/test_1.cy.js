c9892b440af8e0140175d1ed6d7b45c77d7a7d1a
Joanna <joanna.garstka92@gmail.com>
2023-04-26 21:08:38 +0200

    Test

--- cypress/e2e/test_1.cy.js
+++ cypress/e2e/test_1.cy.js
@@ -5,8 +5,8 @@ describe('First test', () => {
         cy.get('#user_email').type('user888@gmail.com'),
         cy.get('#user_password').type('1234567890'),
         cy.get('.eckniwg2').click(),
-        cy.get('#open-navigation-menu-mobile').click(),
-        cy.get(':nth-child(9) > .css-bve2vl').click()
+        cy.get('#open-navigation-menu-mobile').click()
+      //  cy.get(':nth-child(9) > .css-bve2vl').click()
     });
 
     
